3eff01e31411679d8e73c8ca8c00247bcdffd7fa
Sawer <danilch4024@gmail.com>
2025-04-17 20:06:18 +0500

    metrics

--- src/app.py
+++ src/app.py
@@ -33,6 +33,20 @@ class Embedding(db.Model):
         return f'<Embedding {self.student_id}/{self.datedata}>'
 
 
+class CheckLog(db.Model):
+    __tablename__ = 'checkLogs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.BigInteger, nullable=False)
+    datedata = db.Column(db.DateTime, nullable=False)
+    status_code = db.Column(db.Integer, nullable=False)
+    is_match = db.Column(db.Boolean, nullable=False)
+    similarity = db.Column(db.Numeric(5, 3), nullable=False)
+    
+    def __repr__(self):
+        return f'<CheckLog {self.student_id}/{self.datedata} {self.status_code}>'
+
+
 with app.app_context():
     db.session.execute(text('CREATE EXTENSION IF NOT EXISTS vector'))
     db.session.commit()
@@ -69,7 +83,7 @@ def calculate_similarity_with_recent_embeddings(student_id, embedding, limit):
         .limit(limit).all()
     similarity = [calculate_embedding_similarity(np.array(row[0]), embedding) for row in results]
     if similarity:
-        return float(sum(similarity) / len(similarity)), True
+        return round(float(sum(similarity) / len(similarity)), 3), True
     return DEFAULT_SIMILARITY, False
 
 
@@ -87,6 +101,32 @@ def add_embedding_and_clear_overflow(student_id, datedata, embedding, limit):
         .filter(Embedding.id.notin_(subquery)) \
         .delete(synchronize_session=False)
     db.session.commit()
+
+
+def add_check_log(student_id, datedata, status_code, is_match=False, similarity=0):
+    new_log = CheckLog(student_id=student_id, datedata=datedata,
+                       status_code=status_code, is_match=is_match, similarity=similarity)
+    db.session.add(new_log)
+    db.session.commit()
+
+
+def create_metrics(dateFrom=None, dateTo=None):
+    filters = []
+    if dateFrom:
+        filters.append(CheckLog.datedata >= dateFrom)
+    if dateTo:
+        filters.append(CheckLog.datedata <= dateTo)
+
+    return db.session.query(
+        db.func.count().label('total_count'),
+        db.func.count(db.case((CheckLog.status_code == 200, 1))).label('count_status_200'),
+        db.func.count(db.case((CheckLog.status_code == 404, 1))).label('count_status_404'),
+        db.func.count(db.case((CheckLog.status_code == 409, 1))).label('count_status_409'),
+        db.func.count(db.case(((CheckLog.status_code == 200) & (CheckLog.is_match == True), 1))).label('count_is_match_true'),
+        db.func.count(db.case(((CheckLog.status_code == 200) & (CheckLog.is_match == False), 1))).label('count_is_match_false'),
+        db.func.avg(db.case(((CheckLog.status_code == 200) & (CheckLog.is_match == True), CheckLog.similarity))).label('avg_similarity_is_match_true'),
+        db.func.avg(db.case(((CheckLog.status_code == 200) & (CheckLog.is_match == False), CheckLog.similarity))).label('avg_similarity_is_match_false')
+    ).filter(*filters).first()._asdict()
 
 
 @app.route('/students', methods=['GET'])
@@ -114,6 +154,7 @@ def check():
 
     student_id = request.form['student_id']
     file = request.files['file'].read()
+    datedata = datetime.datetime.now(datetime.timezone.utc)
 
     try:
         embedding = process_image(file)
@@ -122,21 +163,27 @@ def check():
             case 0:
                 return jsonify({'error': "Bad image file"}), 400
             case 404:
+                add_check_log(student_id, datedata, 404)
                 return jsonify({'error': "Face is not recognized"}), 404
             case 409:
+                add_check_log(student_id, datedata, 409)
                 return jsonify({'error': "More than one face"}), 409
 
     similarity, checked = calculate_similarity_with_recent_embeddings(student_id, embedding, COMPARE_WITH_LAST_LIMIT)
     is_match = bool(similarity >= THRESHOLD)
 
-    if not is_match:
-        return jsonify({ "checked": checked, "is_match": is_match, "similarity": similarity })
-    
-    datedata = datetime.datetime.now(datetime.timezone.utc)
-    add_embedding_and_clear_overflow(student_id, datedata, embedding, COMPARE_WITH_LAST_LIMIT)
+    if not checked or is_match:
+        add_embedding_and_clear_overflow(student_id, datedata, embedding, COMPARE_WITH_LAST_LIMIT)
+
+    if checked:
+        add_check_log(student_id, datedata, 200, is_match, similarity)
 
     return jsonify({ "checked": checked, "is_match": is_match, "similarity": similarity })
 
+
+@app.route('/metrics', methods=['GET'])
+def metrics():
+    return jsonify(create_metrics())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
