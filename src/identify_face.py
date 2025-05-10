a6fb4d889fb6176d709d8999f31654199dda1df2
Михаил Владимиров <mikhail.vladimirov@urfu.me>
2025-05-11 04:17:50 +0500

    Добавил вторую версию функции получения эмбеддинга. В отличие от прошлой теперь выбирается набольшее по площади и наиболее близкое к центру лицо

--- src/identify_face.py
+++ src/identify_face.py
@@ -28,6 +28,34 @@ def get_face_embedding(file):
     return faces[0].embedding
 
 
+def get_face_embedding2(file):
+    """В отличие от прошлой версии теперь выбирается набольшее по площади и
+    наиболее близкое к центру лицо"""
+    file_array = np.frombuffer(file, dtype=np.uint8)
+    img = cv2.imdecode(file_array, cv2.IMREAD_COLOR)
+    if img is None:
+        raise ValueError(0)  # проблема с изображением
+
+    h, w, _ = img.shape
+    center = np.array([w / 2, h / 2])
+
+    def score(face):
+        x1, y1, x2, y2 = face.bbox
+        face_center = np.array([(x1 + x2) / 2, (y1 + y2) / 2])
+        area = (x2 - x1) * (y2 - y1)
+        dist_to_center = np.linalg.norm(face_center - center)
+
+        # Нормализация: центр ближе — лучше, площадь больше — лучше
+        return area / (1 + dist_to_center)
+
+    faces = app.get(img)
+    if len(faces) < 1:
+        raise ValueError(404)  # нет лица
+    if len(faces) > 1:
+        raise ValueError(409)  # много лиц
+
+    return max(faces, key=score).embedding
+
 def compare_faces(emb1, emb2, threshold=0.65):
     """Compare two embeddings using cosine similarity"""
     similarity = np.dot(emb1, emb2) / (
